468f5ce6217146f8d0326bfc03a738c1f8b5360a
changxinrui <641215320@qq.com>
2021-12-03 18:12:42 +0800

    [add] 细化启动结构

--- Main/Assets/Scripts/Scripts_Mode/Init.cs
+++ Main/Assets/Scripts/Scripts_Mode/Init.cs
@@ -1,15 +1,38 @@
 namespace Mode
 {
+    using System;
     using UnityEngine;
+    using UnityEngine.AddressableAssets;
 
     [DisallowMultipleComponent]
     public class Init : MonoBehaviour
     {
+        public enum AAMode
+        {
+            PreDownload,
+            PlayingDownload,
+        }
+
+        public AAMode mAAMode = AAMode.PreDownload;
+
         private async void Awake()
         {
 #if DEBUG
             await ModeDebugger.Init();
 #endif
+            switch (mAAMode)
+            {
+                case AAMode.PreDownload:
+                    await ModeAddressables.HotUpdate();
+                    break;
+                case AAMode.PlayingDownload:
+                    await ModeAddressables.CheckUpdate();
+                    break;
+                default:
+                    Addressables.LogError("当前AAMode:" + mAAMode + ",未进行对应模式初始化");
+                    throw new ArgumentOutOfRangeException();
+            }
+
             await ModeAddressables.HotUpdate();
             await ModeILRuntime.Init();
 
